0a79d4113b0d349729f5be309aba3c44d0b2f42e
Antoine <ant.delalande@gmail.com>
2024-04-29 23:22:28 +0200

    fixed css

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -18,13 +18,13 @@ function Home() {
         <section className="home-section">
         <div id="container1">
           <img src="" alt="" />
-          <h1>Welcome to Wizard’s Castle, dear adventurer</h1>
+          <h2>Welcome to Wizard’s Castle, dear adventurer</h2>
           <p className="intro-text">
-          Welcome to the Wizard's Library, the definitive repository of knowledge for all aspiring adventurers in the realms of Dungeons & Dragons. Within these hallowed halls, you will find a wealth of arcane wisdom and mystical lore, carefully curated to guide you on your journey through the boundless tapestry of the D&D universe.
+          Welcome to the Wizard's Castle, the definitive repository of knowledge for all aspiring adventurers in the realms of Dungeons & Dragons. Within these hallowed halls, you will find a wealth of arcane wisdom and mystical lore, carefully curated to guide you on your journey through the D&D universe.
           </p>
           <div className="button">
             <Link to="/rules">
-              <h1 className="h1Button">Check the rules</h1>
+              <h3 className="h1Button">Check the rules</h3>
             </Link>
           </div>
         </div>
@@ -37,36 +37,36 @@ function Home() {
         </header>
         <section className="home-section">
         <div id="container2">
-          <h1>Have a peek at our precious resources</h1>
+          <h2>Have a peek at our precious resources</h2>
           <p className="intro-text">
-            Browse our magic library by themes and topics
+          Explore our vast collection of tomes and scrolls, each containing insights into the intricacies of character creation, spellcasting, combat tactics, and more. Delve into the depths of our archives to uncover hidden gems and forgotten secrets, or consult our compendium of rules to clarify any questions that may arise during your adventures.
           </p>
           <ul className="listOption">
             <li>
               <Link to="/rules">
                 <div className="navItemRules">
-                  <h1>Rules</h1>
+                  <h3>Rules</h3>
                 </div>
               </Link>
             </li>
             <li>
               <Link to="/classes">
                 <div className="navItemClass">
-                  <h1>Classes</h1>
+                  <h3>Classes</h3>
                 </div>
               </Link>
             </li>
             <li>
               <Link to="/bestiary">
                 <div className="navItemBestiary">
-                  <h1>Bestiary</h1>
+                  <h3>Bestiary</h3>
                 </div>
               </Link>
             </li>
             <li>
               <Link to="/spells">
                 <div className="navItemSpells">
-                  <h1>Spells</h1>
+                  <h3>Spells</h3>
                 </div>
               </Link>
             </li>
