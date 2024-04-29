8f00098377a95f84435ff8ddfe865940f6ea11b7
Antoine Delalande <ant.delalande@gmail.com>
2024-04-29 17:00:59 +0200

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
         <div>
           <img
             className="imageCenter"
-            src="../assets/images/BattleField.jpg"
+            src="/src/assets/images/BattleField.jpg"
             alt=""
           />
         </div>
